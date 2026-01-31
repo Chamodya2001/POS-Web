ddf28fdd42ac4e1d1c97c8533c44981d12b70325
kavindu chamodya <kavindu@infyinnos.com>
2026-02-01 03:39:39 +0530

    item image uplod seting

--- src/pages/service/AddProductPage_service.js
+++ src/pages/service/AddProductPage_service.js
@@ -3,17 +3,19 @@ import APIHandler from "../../handlers/APIHandler";
 
 export const AddProductPage_service = {
   addProduct,
- 
+  uploadItemImage,
 };
 
+// Add product
 async function addProduct(data) {
   
   const requestOptions = {
     method: "POST",
-    headers: APIHandler.getHeader(config.azure_ad_config.apis.POS.name),
+    headers: APIHandler.getHeader(
+      config.azure_ad_config.apis.POS.name
+    ),
     body: JSON.stringify(data),
   };
-  console.log("URL being called:",requestOptions, `${config.pos_api_url}/api/item/save`);
 
   const response = await fetch(
     `${config.pos_api_url}/api/item/save`,
@@ -23,6 +25,25 @@ async function addProduct(data) {
   return APIHandler.handleResponse(response);
 }
 
+// Upload product image
+async function uploadItemImage(file) {
+  try {
+    const response = await fetch(
+      `${config.pos_api_url}/api/item/upload-image`,
+      {
+        method: "POST",
+        body: file, 
+      }
+    );
 
+    if (!response.ok) {
+      throw new Error(`Upload failed with status ${response.status}`);
+    }
 
-
+    const data = await response.json();
+    return data.data.image_code; 
+  } catch (err) {
+    console.error("Image upload failed:", err);
+    throw err;
+  }
+}
